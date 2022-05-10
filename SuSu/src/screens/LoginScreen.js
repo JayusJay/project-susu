7888add49359fb889e578720984b804ec29c9ddc
Jayus Jay <atayure@gmail.com>
2022-05-10 00:46:19 +0000

    modified:   SuSu/android/app/build.gradle 	new file:   SuSu/src/assets/images/google.svg 	modified:   SuSu/src/screens/LoginScreen.js 	modified:   SuSu/src/styles/loginStyle.js

--- SuSu/src/screens/LoginScreen.js
+++ SuSu/src/screens/LoginScreen.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import {View, Text, Button, Image, StyleSheet, TextInput, TouchableOpacity, Keyboard, Alert, SafeAreaView} from "react-native";
-//import MaterialIcons from "react-native-vector-icons/MaterialIcons"
+import MaterialIcons from "react-native-vector-icons/MaterialIcons"
+import Ionicons from "react-native-vector-icons/Ionicons"
 
 import LoginSVG from "../assets/images/sampleLogo.svg"
+import GoogleSVG from "../assets/images/google.svg"
 import LoginStyle from "../styles/loginStyle";
 
 const LoginScreen = () => {
@@ -15,12 +17,38 @@ const LoginScreen = () => {
         <SafeAreaView style={LoginStyle.SafeAreaView}>
             <View style={LoginStyle.view1}>
                 <View style = {LoginStyle.view2}>
-                    <LoginSVG height = {300} width = {300}/>
+                    <LoginSVG height = {280} width = {280}/>
                 </View>
-                <Text style = {LoginStyle.text}>Login </Text>
-                <View /*style = {LoginStyle.view3}*/>
-                    <TextInput></TextInput>
+                <Text style = {LoginStyle.text}>Login</Text>
+                <View style = {LoginStyle.view3}>
+                    <MaterialIcons name ='alternate-email' size={20} style = {LoginStyle.icon}/>
+                    <TextInput 
+                    underlineColorAndroid="transparent"
+                    placeholder="Email"
+                    style = {LoginStyle.textInput} 
+                    keyboardType = "email-address"/>
                 </View>
+                <View style = {LoginStyle.view3}>
+                    <Ionicons name ='lock-closed-outline' size={20} style = {LoginStyle.icon}/>
+                    <TextInput
+                    underlineColorAndroid="transparent"
+                    placeholder="Password" 
+                    style = {LoginStyle.textInput}
+                    secureTextEntry = {true}/>
+                    <TouchableOpacity onPress={() => {}} style = {LoginStyle.forgotOpacity}>
+                        <Text style = {LoginStyle.forgotPassword}>Forgot Password?</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity onPress={() => {}} style = {LoginStyle.signupOpacity}>
+                    <Text style = {LoginStyle.signupText}>Don't have an account? Sign up</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}} style = {LoginStyle.loginOpacity}>
+                    <Text style = {LoginStyle.loginText}>Login</Text>
+                </TouchableOpacity>
+                <Text style = {LoginStyle.alternate}>Or login with ...</Text>
+                <TouchableOpacity onPress={() => {}} style = {LoginStyle.googleOpacity}>
+                    <GoogleSVG height = {40} width = {40}/>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
--- SuSu/src/styles/loginStyle.js
+++ SuSu/src/styles/loginStyle.js
@@ -6,20 +6,77 @@ const LoginStyle = StyleSheet.create({
         backgroundColor: "#fff",
         justifyContent: "center",
     },
+    text: {
+        fontFamily: "Roboto-medium",
+        fontSize: 28,
+        fontWeight: "500",
+        color: "#AD40AF",
+        marginBottom: 20,
+    },
     view1: {
         paddingHorizontal: 25,
     },
-    view2 : {
+    view2: {
         alignItems: "center",
     },
-    text: {
-        fontFamily: "Roboto-medium",
-        fontSize: 2,
-        fontWeight: "500",
-        color: "#333",
+    view3: {
+        flexDirection: "row",
+        borderBottomColor: "#ccc",
+        borderBottomWidth: 1,
+        paddingBottom: 8,
+        marginBottom: 25,
+    },
+    view4: {
+
+    },
+    icon: {
+        color: "#666",
+        marginRight: 5,
+        paddingVertical: 10,
+    },
+    textInput: {
+        flex: 1,
+        paddingVertical: 0,
+    },
+    forgotOpacity: {
+        marginTop: 15,
+    },
+    forgotPassword: {
+        color: "#AD40AF",
+        fontWeight: "700",
+    },
+    signupText: {
+        color: "#AD40AF",
+        fontWeight: "400"
+    },
+    signupOpacity: {
+        marginTop: -15,
+    },
+    loginOpacity: {
+        backgroundColor: "#AD40AF",
+        padding: 20,
+        borderRadius: 10,
+        marginTop: 25,
+        marginBottom: 5,
+    },
+    loginText: {
+        textAlign: "center",
+        fontWeight: "700",
+        fontSize: 16,
+        color: "#fff",
+    },
+    alternate: {
+        textAlign: "center",
+        color: "#666",
+        marginTop: 10,
         marginBottom: 30,
-    }
-    
+    },
+    googleOpacity: {
+        alignItems: "center",
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+    },
 })
 export default LoginStyle
 
